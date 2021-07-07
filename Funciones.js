a68fcf7b7127c8fe350a4fea29ccc37b51c73aee
ArenillasFelipe <felipearenillas02@gmail.com>
2021-07-06 23:55:29 -0300

    Se finalizo el proyecto

--- Funciones.js
+++ Funciones.js
@@ -15,161 +15,148 @@ function Funciones(id, value) {
     angbeta = document.getElementById("AnguloBeta").value;
 
 
-
     if (A == '' && B == '' && H == '' && angalfa == '' && angbeta == '') {
         alert("Debe ingresar al menos dos datos para calcular")
         Reinicio();
     } else {
 
 
-            if (H == '' && angalfa == '' && angbeta == '') {
-                if (A <= 0 || B <= 0) {
-                    alert("Los datos ingresados son invalidos");
+        if (H == '' && angalfa == '' && angbeta == '') {
+            if (A <= 0 || B <= 0) {
+                alert("Los datos ingresados son invalidos");
+                Reinicio();
+            } else {
+                H = Math.sqrt(A * A + B * B)
+                angbeta = 180 / Math.PI * Math.atan(A / B);
+                angalfa = 180 / Math.PI * Math.atan(B / A);
+
+
+                document.datos.LadoH.value = H.toFixed(2);
+                document.datos.AnguloBeta.value = angbeta.toFixed(2);
+                document.datos.Anguloalfa.value = angalfa.toFixed(2);
+            }
+        }
+        if (B == '' && angalfa == '' && angbeta == '') {
+            if (A <= 0 || H <= 0) {
+                alert("Los datos ingresados son invalidos");
+                Reinicio();
+            } else {
+
+                c = (H * H - A * A)
+                if (c <= 0) {
+                    alert("A no puede ser mayor que H");
                     Reinicio();
                 } else {
-                    H = Math.sqrt(A * A + B * B)
-                    angbeta = 180 / Math.PI * Math.atan(A / B);
-                    angalfa = 180 / Math.PI * Math.atan(B / A);
+                    B = Math.sqrt(c)
+                    angbeta = 180 / Math.PI * Math.asin(A / H);
+                    angalfa = 180 / Math.PI * Math.acos(A / H);
 
-
-                    document.datos.LadoH.value = H.toFixed(2);
+                    document.datos.LadoB.value = B.toFixed(2);
                     document.datos.AnguloBeta.value = angbeta.toFixed(2);
                     document.datos.Anguloalfa.value = angalfa.toFixed(2);
                 }
             }
-            if (B == '' && angalfa == '' && angbeta == '') {
-                if (A <= 0 || H <= 0) {
-                    alert("Los datos ingresados son invalidos");
+        }
+        if (A == '' && angalfa == '' && angbeta == '') {
+            if (B <= 0 || H <= 0) {
+                alert("Los datos ingresados son invalidos");
+                Reinicio();
+            } else {
+                c = (H * H - B * B)
+                if (c <= 0) {
+                    alert("B no puede ser mayor que H");
                     Reinicio();
                 } else {
+                    A = Math.sqrt(c)
+                    angbeta = 180 / Math.PI * Math.acos(B / H);
+                    angalfa = 180 / Math.PI * Math.asin(B / H);
 
-                    c = (H * H - A * A)
-                    if (c <= 0) {
-                        alert("A no puede ser mayor que H");
-                        Reinicio();
-                    } else {
-                        B = Math.sqrt(c)
-                        angbeta = 180 / Math.PI * Math.asin(A / H);
-                        angalfa = 180 / Math.PI * Math.acos(A / H);
-
-                        document.datos.LadoB.value = B.toFixed(2);
-                        document.datos.AnguloBeta.value = angbeta.toFixed(2);
-                        document.datos.Anguloalfa.value = angalfa.toFixed(2);
-                    }
+                    document.datos.LadoA.value = A.toFixed(2);
+                    document.datos.AnguloBeta.value = angbeta.toFixed(2);
+                    document.datos.Anguloalfa.value = angalfa.toFixed(2);
                 }
             }
-            if (A == '' && angalfa == '' && angbeta == '') {
-                if (B <= 0 || H <= 0) {
-                    alert("Los datos ingresados son invalidos");
-                    Reinicio();
-                } else {
-                    c = (H * H - B * B)
-                    if (c <= 0) {
-                        alert("B no puede ser mayor que H");
-                        Reinicio();
-                    } else {
-                        A = Math.sqrt(c)
-                        angbeta = 180 / Math.PI * Math.acos(B / H);
-                        angalfa = 180 / Math.PI * Math.asin(B / H);
+        }
+        if (B == '' && H == '' && angalfa == '') {
+            angbeta = angbeta * Math.PI / 180
+            H = A / Math.sin(angbeta);
+            B = A / Math.tan(angbeta);
+            angalfa = 180 - angbeta * 180 / Math.PI - 90;
 
-                        document.datos.LadoA.value = A.toFixed(2);
-                        document.datos.AnguloBeta.value = angbeta.toFixed(2);
-                        document.datos.Anguloalfa.value = angalfa.toFixed(2);
-                    }
-                }
-            }
-            if (B == '' && H == '' && angalfa == '') {
-                angbeta = angbeta * Math.PI / 180
-                H = A / Math.sin(angbeta);
-                B = A / Math.tan(angbeta);
-                angalfa = 180 - angbeta * 180 / Math.PI - 90;
+            document.datos.LadoH.value = H.toFixed(2);
+            document.datos.LadoB.value = B.toFixed(2);
+            document.datos.Anguloalfa.value = angalfa.toFixed(2);
+        }
 
-                document.datos.LadoH.value = H.toFixed(2);
-                document.datos.LadoB.value = B.toFixed(2);
-                document.datos.Anguloalfa.value = angalfa.toFixed(2);
-            }
+        if (B == '' && angbeta == '' && H == '') {
+            angalfa = angalfa * Math.PI / 180
+            H = A / Math.cos(angalfa);
+            B = Math.tan(angalfa) * A;
+            angbeta = 180 - angalfa * 180 / Math.PI - 90;
 
-            if (B == '' && angbeta == '' && H == '') {
-                angalfa = angalfa * Math.PI / 180
-                H = A / Math.cos(angalfa);
-                B = Math.tan(angalfa) * A;
-                angbeta = 180 - angalfa * 180 / Math.PI - 90;
+            document.datos.LadoH.value = H.toFixed(2);
+            document.datos.LadoB.value = B.toFixed(2);
+            document.datos.AnguloBeta.value = angbeta.toFixed(2);
 
-                document.datos.LadoH.value = H.toFixed(2);
-                document.datos.LadoB.value = B.toFixed(2);
-                document.datos.AnguloBeta.value = angbeta.toFixed(2);
+        }
 
-            }
+        if (A == '' && angalfa == '' && H == '') {
+            angbeta = angbeta * Math.PI / 180
+            A = Math.tan(angbeta) * B;
+            H = B / Math.cos(angbeta);
+            angalfa = 180 - angbeta * 180 / Math.PI - 90;
 
-            if (A == '' && angalfa == '' && H == '') {
-                angbeta = angbeta * Math.PI / 180
-                A = Math.tan(angbeta) * B;
-                H = B / Math.cos(angbeta);
-                angalfa = 180 - angbeta * 180 / Math.PI - 90;
+            document.datos.LadoA.value = A.toFixed(2);
+            document.datos.LadoH.value = H.toFixed(2);
+            document.datos.Anguloalfa.value = angalfa.toFixed(2);
+        }
 
-                document.datos.LadoA.value = A.toFixed(2);
-                document.datos.LadoH.value = H.toFixed(2);
-                document.datos.Anguloalfa.value = angalfa.toFixed(2);
-            }
+        if (A == '' && angbeta == '' && H == '') {
+            angalfa = angalfa * Math.PI / 180
+            A = B / Math.tan(angalfa);
+            H = B / Math.sin(angalfa);
+            angbeta = 180 - angalfa * 180 / Math.PI - 90;
 
-            if (A == '' && angbeta == '' && H == '') {
-                angalfa = angalfa * Math.PI / 180
-                A = B / Math.tan(angalfa);
-                H = B / Math.sin(angalfa);
-                angbeta = 180 - angalfa * 180 / Math.PI - 90;
+            document.datos.LadoA.value = A.toFixed(2);
+            document.datos.LadoH.value = H.toFixed(2);
+            document.datos.AnguloBeta.value = angbeta.toFixed(2);
+        }
 
-                document.datos.LadoA.value = A.toFixed(2);
-                document.datos.LadoH.value = H.toFixed(2);
-                document.datos.AnguloBeta.value = angbeta.toFixed(2);
-            }
+        if (A == '' && angalfa == '' && B == '') {
+            angbeta = angbeta * Math.PI / 180
+            B = Math.sin(angbeta) * H;
+            A = Math.cos(angbeta) * H;
+            angalfa = 180 - angbeta * 180 / Math.PI - 90;
 
-            if (A == '' && angalfa == '' && B == '') {
-                angbeta = angbeta * Math.PI / 180
-                B = Math.sin(angbeta) * H;
-                A = Math.cos(angbeta) * H;
-                angalfa = 180 - angbeta * 180 / Math.PI - 90;
+            document.datos.LadoB.value = B.toFixed(2);
+            document.datos.LadoA.value = A.toFixed(2);
+            document.datos.Anguloalfa.value = angalfa.toFixed(2);
+        }
+        if (A == '' && angbeta == '' && B == '') {
+            angalfa = angalfa * Math.PI / 180
+            B = Math.sin(angalfa) * H;
+            A = Math.cos(angalfa) * H;
+            angbeta = 180 - angalfa * 180 / Math.PI - 90;
 
-                document.datos.LadoB.value = B.toFixed(2);
-                document.datos.LadoA.value = A.toFixed(2);
-                document.datos.Anguloalfa.value = angalfa.toFixed(2);
-            }
-            if (A == '' && angbeta == '' && B == '') {
-                angalfa = angalfa * Math.PI / 180
-                B = Math.sin(angalfa) * H;
-                A = Math.cos(angalfa) * H;
-                angbeta = 180 - angalfa * 180 / Math.PI - 90;
-
-                document.datos.LadoB.value = B.toFixed(2);
-                document.datos.LadoA.value = A.toFixed(2);
-                document.datos.AnguloBeta.value = angbeta.toFixed(2);
+            document.datos.LadoB.value = B.toFixed(2);
+            document.datos.LadoA.value = A.toFixed(2);
+            document.datos.AnguloBeta.value = angbeta.toFixed(2);
 
 
-            }
-        if(A<=0 || B<=0 || H<=0 || angbeta<=0 || angalfa<=0){
+        }
+        if (A <= 0 || B <= 0 || H <= 0 || angbeta <= 0 || angalfa <= 0) {
             alert("Los datos ingresados no son validos")
             Reinicio();
         }
 
-        }
-
-    H=H*10;
-    A=A*10;
-    B=B*10;
-
-
-
     }
 
+    H = H * 10;
+    A = A * 10;
+    B = B * 10;
 
 
-
-
-
-
-
-
-
-
+}
 
 
 /**
@@ -181,7 +168,7 @@ function Funciones(id, value) {
 
 
 
-function Reinicio(id, value){
+function Reinicio(id, value) {
 
     document.datos.LadoA.value = null;
     document.datos.LadoB.value = null;
@@ -194,19 +181,18 @@ function Reinicio(id, value){
     var ctx = canvas.getContext("2d");
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    x=0;
-    y=0;
-    dx=0.8;
-    dy=0.8;
-R=1;
+    x = 0;
+    y = 0;
+    dx = 0.8;
+    dy = 0.8;
+    R = 1;
 }
-x=0;
-dx=0.8;
-y=0;
-dy=0.8;
-R=0;
 
-
+x = 0;
+dx = 0.8;
+y = 0;
+dy = 0.8;
+R = 0;
 
 
 /**
@@ -221,8 +207,6 @@ function dibujar() {
     if (R == 0) {
 
 
-
-
         var canvas = document.getElementById("mycanvas");
         var ctx = canvas.getContext("2d");
 
@@ -232,8 +216,7 @@ function dibujar() {
 
         ctx.beginPath();
 
-        if(x <= B) {
-
+        if (x <= B) {
 
 
             ctx.moveTo(10, yMax - 10);
@@ -242,19 +225,19 @@ function dibujar() {
             ctx.stroke()
         }
         if (x >= B) {
-            ctx.moveTo(x+10, yMax - 10);
-            ctx.lineTo(x+10, yMax-20 - y);
+            ctx.moveTo(x + 10, yMax - 10);
+            ctx.lineTo(x + 10, yMax - 20 - y);
             ctx.closePath();
             ctx.stroke()
 
         }
-        if(x<B) {
+        if (x < B) {
             x += dx;
-        }else {
-            if (y < A - dy*2) {
+        } else {
+            if (y < A - dy * 2) {
                 y += dy;
             } else {
-                ctx.moveTo(x + 10, yMax-20 - y);
+                ctx.moveTo(x + 10, yMax - 20 - y);
                 ctx.lineTo(10, yMax - 10);
                 ctx.closePath();
                 ctx.stroke();
