7e14e6eefa211bdaa4ec14a9443a21f4bdfce676
ArenillasFelipe <felipearenillas02@gmail.com>
2021-07-06 21:37:27 -0300

    Se avanzo

--- Funciones.js
+++ Funciones.js
@@ -1,5 +1,5 @@
 function Funciones(id, value) {
-
+    R=0;
     A = document.getElementById("LadoA").value;
     B = document.getElementById("LadoB").value;
     H = document.getElementById("LadoH").value;
@@ -141,22 +141,74 @@ function Reinicio(id, value){
     document.datos.AnguloBeta.value = null;
     document.datos.AnguloTita.value = null;
     document.datos.Anguloalfa.value = null;
-}
-
-
-
-
-
-
-function dibujartrian(){
 
     var canvas = document.getElementById("mycanvas");
     var ctx = canvas.getContext("2d");
 
-
-var yMax = canvas.height;
-var margen = 5;
-ctx.fillStyle = "#000000";
-ctx.fillRect(0, yMax-40-margen, 40, 40)
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    x=0;
+    y=0;
+    dx=0.4;
+    dy=0.4;
+    c=0;
+R=1;
 }
+x=0;
+dx=0.4;
+y=0;
+dy=0.4;
+c=0;
+R=0;
+function dibujar() {
+    if (R == 0) {
+        if(c==0){
+            B = B * 10;
+            A = A * 10;
+        }
+        c=1;
+
+
+
+
+        var canvas = document.getElementById("mycanvas");
+        var ctx = canvas.getContext("2d");
+
+        var yMax = canvas.height;
+        var xMax = canvas.width - 10;
+
+
+        ctx.beginPath();
+
+        if(x <= B) {
+
+
+
+            ctx.moveTo(10, yMax - 10);
+            ctx.lineTo(x + 10, yMax - 10);
+            ctx.closePath();
+            ctx.stroke()
+        }
+        if (x >= B) {
+            ctx.moveTo(x+10, yMax - 10);
+            ctx.lineTo(x+10, yMax-20 - y);
+            ctx.closePath();
+            ctx.stroke()
+
+        }
+        if(x<B) {
+            x += dx;
+        }else {
+            if (y < A - dy*2) {
+                y += dy;
+            } else {
+                ctx.moveTo(x + 10, yMax-20 - y);
+                ctx.lineTo(10, yMax - 10);
+                ctx.closePath();
+                ctx.stroke();
+            }
+        }
+
+
+    }
+}
+
